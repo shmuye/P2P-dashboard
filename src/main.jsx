f520189de4e23cea2c8dc45077e4ba19f5e9e00b
Shmuye <shmuye27@gmail.com>
2025-04-19 07:35:45 +0300

    data.json file created

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import App from './App'
+import ReactDom from 'react-dom/client'
+import './index.css'
+const root = ReactDom.createRoot(document.getElementById('root'))
+root.render(<App />)
